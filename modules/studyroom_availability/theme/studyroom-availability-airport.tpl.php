653f4503a6e9498317a9b21074b3526d26bf950d
Tim <timsmith@ou.edu>
2017-10-03 11:00:32 -0500

    graduate map and conditional images

--- modules/studyroom_availability/theme/studyroom-availability-airport.tpl.php
+++ modules/studyroom_availability/theme/studyroom-availability-airport.tpl.php
@@ -19,7 +19,24 @@ $args = explode('/', drupal_get_path_alias());
 <div class="airport-page">
     <?php
     if ($args[3] == 'facultymap'){ ?>
+      <div class="select-room-instruction">
+          Select a room to make a reservation.
+      </div>
         <img src="/sites/all/modules/studyroom/modules/studyroom_availability/theme/img/LL2_faculty_airport.gif"
+             usemap="#facultymap2"/>
+        <map name="facultymap2">
+            <area shape="poly" coords="59,389,119,389,122,458,61,445" href="https://libraries.webtest.lib.ou.edu/space/1" target="_blank" alt="LL2531">
+            <area shape="poly" coords="126,388,186,390,185,468,125,460" href="https://libraries.webtest.lib.ou.edu/space/2" target="new" alt="LL2533">
+            <area shape="poly" coords="192,388,253,389,252,476,189,469" href="https://libraries.webtest.lib.ou.edu/space/3" target="new" alt="LL2535">
+            <area shape="poly" coords="259,389,319,388,319,481,260,476,259,478" href="https://libraries.webtest.lib.ou.edu/space/4" target="new" alt="LL2537">
+            <area shape="poly" coords="321,387,384,389,384,482,322,481" href="https://libraries.webtest.lib.ou.edu/space/5" target="new" alt="LL2539">
+        </map>
+     <?php }
+     else if ($args[3] == 'graduatemap'){?>
+       <div class="select-room-instruction">
+       Select a room to make a reservation.
+      </div>
+        <img src="/sites/all/modules/studyroom/modules/studyroom_availability/theme/img/LL2_graduate_airport.gif"
              usemap="#facultymap2"/>
         <map name="facultymap2">
             <area shape="poly" coords="59,389,119,389,122,458,61,445" href="https://libraries.webtest.lib.ou.edu/space/1" target="_blank" alt="LL2531">
