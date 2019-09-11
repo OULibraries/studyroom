4488f7355abf83164a52be5a2c1c4ea98338d0f5
kdaviswx <kdaviswx@ou.edu>
2019-09-11 16:35:59 -0500

    adding code for multi-day option by location

--- js/studyroom_date_popup.js
+++ js/studyroom_date_popup.js
@@ -31,4 +31,39 @@
       }
     }
   };
+
+  jQuery(document).ready(function () {
+    if ($('#edit-space-id').val()) {
+      var multi_day_location_list = [21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44];
+
+      var is_faculty_or_admin = $('body').hasClass('page-reservation-add-admin') || $('body').hasClass('page-reservation-add-faculty') ? 1 : 0;
+
+      if (is_faculty_or_admin === 1) {
+        var space_id = parseInt($('#edit-space-id').val());
+
+        if (multi_day_location_list.includes(space_id)) {
+          $('div.field-name-field-multi-reservation-date-tim').show();
+          $('div.field-name-field-reservation-datetime').hide();
+        }
+        else {
+          $('div.field-name-field-multi-reservation-date-tim').hide();
+          $('div.field-name-field-reservation-datetime').show();
+        }
+
+        $('#edit-space-id').on('change', function () {
+          space_id = parseInt($('#edit-space-id').val());
+          if (multi_day_location_list.includes(space_id)) {
+            $('div.field-name-field-multi-reservation-date-tim').show();
+            $('div.field-name-field-reservation-datetime').hide();
+          } else {
+            $('div.field-name-field-multi-reservation-date-tim').hide();
+            $('div.field-name-field-reservation-datetime').show();
+          }
+        });
+      } else {
+        $('div.field-name-field-multi-reservation-date-tim').hide();
+        $('div.field-name-field-reservation-datetime').show();
+      }
+    }
+  });
 })(jQuery);
