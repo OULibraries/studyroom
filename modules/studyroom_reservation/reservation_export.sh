d41e607bc64a9f3a7bcaacac68089e85b57f6674
kdaviswx <kdaviswx@ou.edu>
2019-12-10 11:06:05 -0600

    New function for reservation deletion. Changes to shell script for cron automation process.

--- modules/studyroom_reservation/reservation_export.sh
+++ modules/studyroom_reservation/reservation_export.sh
@@ -4,7 +4,7 @@
 echo "Exporting reservation data..."
 
 ## Set SITENAME based on first param provided
-SITENAME="libraries"
+SITENAME="kdlibraries"
 
 ## set DRUSH PATH and change to the site's drupal dir
 cd /srv/$SITENAME/drupal || exit
@@ -13,24 +13,25 @@ cd /srv/$SITENAME/drupal || exit
 RESFILE="/var/log/roomres/${SITENAME}_roomres_${HOSTNAME}_output-$(date "+%Y%m%d_%I:%M:%S").csv"
 
 ## Run drush command to export query results to a csv file in /var/log/roomres
-echo "reservation_id, reservation_type, space_id, user_id, duration, created, deleted, reservation_datetime, space_type, label" > "$RESFILE"
-drush sql-query "SELECT r.reservation_id, r.type AS reservation_type, r.space_id, r.uid user_id, r.duration, r.created, rd.deleted, rd.field_reservation_datetime_value reservation_datetime, s.type, s.label FROM studyroom_reservation r LEFT JOIN field_data_field_reservation_datetime rd ON rd.entity_id = r.reservation_id LEFT JOIN studyroom_space s ON s.space_id = r.space_id WHERE rd.field_reservation_datetime_value < date_sub(now(), interval 1 month)" | awk 'BEGIN { FS = "\t"} ; {print "\""$1"\",\""$2"\",\""$3"\",\""$4"\",\""$5"\",\""$6"\",\""$7"\",\""$8"\",\""$9"\",\""$10"\""}' >> "$RESFILE"
+echo "reservation_id, reservation_type, space_id, user_id, duration, created, deleted, reservation_datetime, space_type, label" > "$RESFILE" | drush sql-query "SELECT r.reservation_id, r.type AS reservation_type, r.space_id, r.uid user_id, r.duration, r.created, rd.deleted, rd.field_reservation_datetime_value reservation_datetime, s.type, s.label FROM studyroom_reservation r LEFT JOIN field_data_field_reservation_datetime rd ON rd.entity_id = r.reservation_id LEFT JOIN studyroom_space s ON s.space_id = r.space_id WHERE rd.field_reservation_datetime_value < date_sub(now(), interval 1 month)" | awk 'BEGIN { FS = "\t"} ; {print "\""$1"\",\""$2"\",\""$3"\",\""$4"\",\""$5"\",\""$6"\",\""$7"\",\""$8"\",\""$9"\",\""$10"\""}' >> "$RESFILE"
 
 echo "Data exported to ${RESFILE}"
 
 echo "Verify csv file exists..."
-if [[ $(wc -l "$RESFILE") > 2 ]]; then
-    echo "$RESFILE exists and is more than 2 lines"
+
+if read -r && read -r
+then
+  echo "$RESFILE exists and is more than 1 line"
 
     echo "Delete data from db..."
 
-    ## Query data again to remove from db.
-    drush sql-query "DELETE A.*, B.* FROM field_data_field_reservation_datetime A INNER JOIN studyroom_reservation B ON A.entity_id = B.reservation_id WHERE B.reservation_id IN (SELECT entity_id FROM field_data_field_reservation_datetime WHERE field_reservation_datetime_value < date_sub(now(), interval 1 month))"
+    ## send csv file name to function in studyroom_reservation.admin.inc to remove the nodes.
+    drush php-eval "module_load_include('inc', 'studyroom_reservation', 'studyroom_reservation.admin'); studyroom_reservation_delete_by_csv('${RESFILE}');"
 
     echo "Data deleted."
 else
   echo "Reservations csv file not properly exported, attention required!"
   exit 1;
-fi
+fi < "$RESFILE"
 
 echo "Reservation export and deletion complete."
